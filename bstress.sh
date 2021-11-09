3542d72a71e6bcec0e1b2d8c9af4a22ac1349941
miyagi-sensei <60123481+miyagi-sensei@users.noreply.github.com>
2021-11-09 15:56:04 +0800

    Update bstress.sh

--- bstress.sh
+++ bstress.sh
@@ -2,7 +2,7 @@
 
 # solution.cpp          solution you want to stress test
 # brute.cpp/correct.py    correct solution for the problem
-# gen.py                generates a new random test case for the problem
+# genb.py                generates a new random test case for the problem
 
 # compile all programs first
 g++ solution.cpp
@@ -23,7 +23,7 @@ done
 # more random testing
 while true 
 do
-	python3 gen.py > in
+	python3 genb.py > in
 	./a.out < in > out
 	# python3 correct.py < in > out_correct
 	./brute < in > out_correct
